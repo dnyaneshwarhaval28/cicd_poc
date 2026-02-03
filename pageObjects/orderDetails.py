77abd88a35c90f70a241be893bea07236d97b6db
Dnyaneshwar Haval <dnyaneshwar.haval@infobeans.com>
2026-02-03 23:41:00 +0530

    code change

--- pageObjects/orderDetails.py
+++ pageObjects/orderDetails.py
@@ -12,7 +12,7 @@ class OrderDetailsPage:
         # self.page.locator("//input[@type='checkbox']").check()
         # self.page.locator("//input[@type='submit']").click()
         self.page.get_by_text("I agree with the term &").click()
-        self.page.get_by_text("Close").click()
+        # self.page.get_by_text("Close").click()
         self.page.get_by_role("button", name="Purchase").click()
         self.verifyOrderMessage()
 
