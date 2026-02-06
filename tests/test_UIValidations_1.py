30b9141c14d2088f59f21c448884dde08e4308c7
Dnyaneshwar Haval <dnyaneshwar.haval@infobeans.com>
2026-02-06 11:49:57 +0530

    allure report fixing - 1

--- tests/test_UIValidations_1.py
+++ tests/test_UIValidations_1.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import expect
 from pageObjects.login import LoginPage
+import allure
 
+@allure.title("Login Test")
+@allure.description("Verify login functionality")
 def test_UIValidationStaticScript(browserInstance):
     #iphone X, Nokia Edge, Blackberry -> verify 3 items are showing in cart.
     loginPage = LoginPage(browserInstance)
